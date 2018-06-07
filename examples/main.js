317b9574559e82b3f818be0a14fbdda6cf68269c
François Pluchino <francois.pluchino@gmail.com>
2018-06-07 12:01:01 +0200

    Replace grunt by webpack

---
+++ examples/main.js
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the Fxp package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'footable/css/footable.core.css';
+import '../js/footable-responsive';
+
+$( '#table-id' ).footable({responsive: {enabled: true}});
